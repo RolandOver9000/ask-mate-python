d9b344ac109a9965320d9427f2c67b6675aa2e6a
Ben Polgardy <benpolgardy@gmail.com>
2019-09-09 18:24:24 +0200

    add display_answers functionality to display_question()

--- server.py
+++ server.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 
 @app.route('/question/<question_id>')
-def display_question(question_id):
+def display_question_and_answers(question_id):
     question_to_display = connection.get_csv_data(data_id=str(question_id))
-    return render_template('question.html', question_to_display=question_to_display)
+    answers_to_display = connection.get_csv_data(answer=True, data_id=str(question_id))
+    return render_template('question.html', question_to_display=question_to_display, answers_to_display=answers_to_display)
 
 
 if __name__ == '__main__':
