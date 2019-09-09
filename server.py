c22a8872c58c5f4430f39bd09c5234fae69cfaaf
Ben Polgardy <benpolgardy@gmail.com>
2019-09-09 18:13:03 +0200

    add display_question() function and route

--- server.py
+++ server.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
+import connection
 
 app = Flask(__name__)
 
 
+@app.route('/question/<question_id>')
+def display_question(question_id):
+    question_to_display = connection.get_csv_data(data_id=str(question_id))
+    return render_template('question.html', question_to_display=question_to_display)
 
 
 if __name__ == '__main__':
