29032a50b3c5cd592901585f4890f4ea28fb9a95
RolandOver9000 <csosz.roland2@gmail.com>
2019-09-10 09:35:23 +0200

    first version of new_answer

--- connection.py
+++ connection.py
@@ -1,4 +1,8 @@
+from flask import Flask, render_template, request, redirect
 import csv
+import time
+
+app = Flask(__name__)
 
 ANSWER_PATH = "sample_data/answer.csv"
 QUESTION_PATH = "sample_data/question.csv"
@@ -24,3 +28,27 @@ def get_csv_data(answer=False, data_id=None):
             data_from_csv.append(data_row)
 
     return data_from_csv
+
+
+@app.route("/question/<question_id>/new-answer", methods=["GET", "POST"])
+def post_an_answer(question_id):
+
+    question_data = get_csv_data(False, question_id)
+    if request.method == "POST":
+        answer = {}
+        answer["answer_id"] = get_next_anwser_id()
+        answer["submission_time"] = int(time.time())
+        answer["vote_number"] = 0
+        answer["question_id"] = question_data["question_id"]
+        answer["message"] = request.form["get_answer"]
+        return redirect("/")
+    else:
+        answer_for_question = get_answer_data(question_id)
+        return render_template("new_answer.html", question=question_data, answer_for_question=answer_for_question)
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True, # Allow verbose error reports
+        port=5000 # Set custom port
+    )
