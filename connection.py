ba7029749abd0c2ea4dd86080c1d0455429b2e1a
Ben Polgardy <benpolgardy@gmail.com>
2019-09-11 13:22:13 +0200

    add get_list_of_ids()

--- connection.py
+++ connection.py
@@ -132,3 +132,16 @@ def get_latest_id_from_csv(answer=False):
         return csv_data[-1]['id']
     else:
         return 0
+
+
+def get_list_of_ids(answer=False):
+    list_of_ids=[]
+    if answer:
+        csv_data = get_csv_data(answer=True)
+    else:
+        csv_data = get_csv_data()
+
+    for row in csv_data:
+        list_of_ids.append(row['id'])
+
+    return list_of_ids
