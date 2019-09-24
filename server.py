9e021313e622a90a112095f77e9453c0096b6238
Ben Polgardy <benpolgardy@gmail.com>
2019-09-24 20:35:24 +0200

    implement edited_count increment in route_edit_comment()

--- server.py
+++ server.py
@@ -197,7 +197,8 @@ def route_edit_comment(comment_id):
     if request.method == 'GET':
         return render_template('new_comment.html', comment=comment_data, answer=answer_data, question=question_data)
 
-    updated_comment = {'message': request.form['comment']}
+    updated_comment = {'message': request.form['comment'],
+                       'edited_count': comment_data['edited_count'] + 1}
     data_manager.update_entry('comment', comment_id, updated_comment)
 
     return redirect(url_for('display_question_and_answers', question_id=question_id))
