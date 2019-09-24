ad7d3f3c72c8b6884d6bbed0673c66ad95d5a0ea
Ben Polgardy <benpolgardy@gmail.com>
2019-09-24 19:03:38 +0200

    implement route_edit_comment()

--- server.py
+++ server.py
@@ -184,5 +184,19 @@ def delete_comment(question_id, answer_id, comment_id):
     return redirect(url_for('display_question_and_answers', question_id=question_id))
 
 
+@app.route('/comments/<comment_id>/edit')
+def route_edit_comment(comment_id):
+    comment_data = data_manager.get_single_entry('comment', comment_id)
+    question_id = comment_data.get('question_id')
+
+    if request.method == 'GET':
+        return render_template('new_comment.html', comment=comment_data)
+
+    user_inputs_for_comment = request.form.to_dict()
+    data_manager.update_entry('comment', comment_id, user_inputs_for_comment)
+
+    return redirect(url_for('display_question_and_answers', question_id=question_id))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
