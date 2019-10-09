6a676bd1600dbb34b29bd48c8825beca462f558a
sandormatyas <sandormatyas7@gmail.com>
2019-10-09 12:58:41 +0200

    add tag security

--- server.py
+++ server.py
@@ -172,27 +172,21 @@ def route_edit_answer(answer_id):
 @app.route('/question/<question_id>/new-tag', methods=["GET", "POST"])
 def route_new_tag(question_id):
     existing_tags = data_manager.get_existing_tags_for_question(question_id, )
+    if data_manager.question_belongs_to_user(session.get('username'), question_id):
+        return render_template('database_ops/new_tag.html', existing_tags=existing_tags)
 
     if request.method == "POST":
         new_tag = request.form.get('new_tag')
         existing_tag_id = request.form.get('existing_tag')
         data_manager.handle_tag(question_id, new_tag, existing_tag_id)
-        return redirect(url_for('display_question_and_answers', question_id=question_id), code=307)
 
-    if 'user_id' in session:
-        user_id_for_question = data_manager.get_user_id_for_question(question_id)
-        if session['user_id'] == user_id_for_question:
-            return render_template('database_ops/new_tag.html', existing_tags=existing_tags)
-
-    return redirect(url_for('display_question_and_answers', question_id=question_id))
+    return redirect(url_for('display_question_and_answers', question_id=question_id), code=307)
 
 
 @app.route('/question/<question_id>/tag/<tag_id>/delete')
 def route_delete_tag(question_id, tag_id):
-    if 'user_id' in session:
-        user_id_for_question = data_manager.get_user_id_for_question(question_id)
-        if session['user_id'] == user_id_for_question:
-            data_manager.delete_tag(question_id, tag_id)
+    if data_manager.question_belongs_to_user(session.get('username'), question_id):
+        data_manager.delete_tag(question_id, tag_id)
 
     return redirect(url_for('display_question_and_answers', question_id=question_id))
 
